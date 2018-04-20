11a77d84c65f45dd5a8f174c041b392b9268c047
Shaynerrr <shayne.tremblay27@gmail.com>
2018-04-20 08:25:18 -0400

    Lab 11

---
+++ Lab semaine 11/Lab11 Superhero/Exercice 1 et 2/Lab5.js
@@ -0,0 +1,103 @@
+
+var Iron= document.getElementById("Iron");
+var Hul = document.getElementById("Hul");
+var Spidy = document.getElementById("Spidy");
+var Veno = document.getElementById("Veno");
+var Pant = document.getElementById("Pant");
+var goBack = document.getElementById("Gb");
+
+function num1()
+{
+document.getElementsByTagName("aside")[0].innerHTML += "NomHero: Iron-Man";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "prenom : Tony";		
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Nom : Stark";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : Terre";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : Terre(New-York)";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Caracteristique: Tony est un génie et son intelligence lui permet de crée des système ultra sophistiqué. Il se spécialise dans le développement d'armes et d'armures.";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Pouvoirs: Grâce a son armure il peut voler, tirer des missiles et plein de fonctionnalités lui permattant de vaincre ses adversaires a tout coup.";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Alliance: Avengers";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Ennemies: Madame Masque, Iron Monger, Mandarin";
+}
+function num2()
+{
+document.getElementsByTagName("aside")[0].innerHTML += "NomHero: Hulk";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "prenom : Robert";		
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Nom : Banner";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : Terre";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Caracteristique: En humain il est un genie en physique nucleaire. Par contre lorsqu'il se transforme il devient incontrollable.";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Pouvoirs: Force quasi illimité et 'invulnérabilité il atteint un poid de 1040-1400 lbs";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Alliance: Avengers";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Ennemies: The Leader, Abomination, Brian Banner";		
+}
+function num3()
+{
+document.getElementsByTagName("aside")[0].innerHTML += "NomHero: Spiderman";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "prenom : Peter";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Nom : Parker";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : Terre(New-York)";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Caracteristique: Étudiant génie";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Pouvoirs: Grimper et tisser des toiles d'araigné pour voler.Une force augmenter",
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Alliance: Avengers";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Ennemies: Venom, Le gobelin  vert, Dr.Octopus, Rhyno";
+}
+function num4()
+{
+document.getElementsByTagName("aside")[0].innerHTML += "NomHero: Venom";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "prenom : Eddie";		
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Nom : Brock";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : La substance est de l'espace";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Caracteristique: Veux ce venger de SPIDER-MAN";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Pouvoirs: Grimper et tisser des toiles d'araigné pour voler.Une force augmenter de SPIDER-MAN";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Alliance: Aucune";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Ennemies: Spider-Man";
+}
+function num5()
+{
+document.getElementsByTagName("aside")[0].innerHTML += "NomHero: Black-Panther";	
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "prenom : T";		
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Nom : Challa";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Lieu : Wakanda";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Caracteristique: C'est un brillant tacticien, scientifique, maître de toute forme de combat a main nu";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Pouvoirs: Les senses de Black Panther et ses capacités physiques son développer contrairement au humain";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Alliance: Avengers , Auparavant les 4 fantastiques, l'armé du Wakanda";
+document.getElementsByTagName("aside")[0].innerHTML += "<br>";
+document.getElementsByTagName("aside")[0].innerHTML += "Ennemies: Erik Killmonger, Klaw, Man-Ape, White Wolf";
+}	
+function delet(){
+    location.reload();
+}				
